af6758026d66e2f2e3309ec5a4cd60d4526d1a9a
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-08-09 01:58:36 -0400

    updated the Home Page

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../assets/global.css";
 import services1 from "../assets/services1.png";
 import services2 from "../assets/services2.png";
-import services3 from "../assets/services3.jpg";
+import services3 from "../assets/services3.png";
 import logo from "../assets/MediBridge_logo.png";
 import { Link } from "react-router-dom";
 const LandingPage = () => {
@@ -38,7 +38,6 @@ const LandingPage = () => {
       <div className="user-authentication fade-in">
         {/* <!-- Show login or account-related links here --> */}
         <div className="login-signup">
-          <Link to={'doctor/dashboard'}>Login as a Doctor</Link>
           <Link to={'patient/dashboard'}>Login as a Patient</Link>
         </div>
         {/* <!-- Display user name or profile picture here after authentication --> */}
@@ -51,30 +50,35 @@ const LandingPage = () => {
           {/* <!-- Add more content and features as needed --> */}
         </section>
         {/* <!-- Featured services or benefits section --> */}
-        <div className="accordion">
-          <ul>
-            <li>
-              <div>
-                <img src={services1} alt="Service 1" />
-                <h2>Secure Payment</h2>
-                <p>Secure payment from patients to doctors based on the service provided.</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={services2} alt="Service 2" />
-                <h2>Electronic Health Records</h2>
-                <p>Access and manage your health records securely online, anytime, anywhere.</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <img src={services3} alt="Service 3" />
-                <h2>Appointment Scheduling</h2>
-                <p>Book and manage appointments with healthcare providers in a convenient and hassle-free way.</p>
-              </div>
-            </li>
-          </ul>
+        {/* Featured services or benefits section */}
+        <div className="features-container">
+          <div className="feature">
+            <div className="feature-content">
+              <h2>Secure Payment</h2>
+              <p>At MediBridge, we leverage the transformative power of Near blockchain technology to take your health data protection to the next level. Our platform ensures that your valuable health information stays tamper-proof, fortified with robust encryption, and protected like never before.</p>
+            </div>
+            <div className="feature-image">
+              <img src={services1} alt="Service 1" />
+            </div>
+          </div>
+          <div className="feature">
+            <div className="feature-content">
+              <h2>Electronic Health Records</h2>
+              <p>Access and manage your health records securely online, anytime, anywhere.Our intuitive and easy-to-navigate platform puts you in control of your health data, making it effortless to manage and share with healthcare providers and researchers.</p>
+            </div>
+            <div className="feature-image">
+              <img src={services2} alt="Service 2" />
+            </div>
+          </div>
+          <div className="feature">
+            <div className="feature-content">
+              <h2>Empowering Medical Research</h2>
+              <p>Join a global community committed to advancing medical knowledge. By sharing your health data, you become an integral part of groundbreaking research that benefits all of humanity.</p>
+            </div>
+            <div className="feature-image">
+              <img src={services3} alt="Service 3" />
+            </div>
+          </div>
         </div>
       </main>
       <footer className="fade-in">
