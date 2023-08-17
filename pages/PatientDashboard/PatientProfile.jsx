c7cf00b3036dfae776afa5e0ed34ed16ffc645bb
Prakhar <prakharojha12@gmail.com>
2023-08-17 22:02:29 +0530

    Integration for Profile page done

--- pages/PatientDashboard/PatientProfile.jsx
+++ pages/PatientDashboard/PatientProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; // Import the Link component
 import RecordsTab from "./RecordsTab";
 import AllergiesTab from "./AllergiesTab";
@@ -6,129 +6,175 @@ import ImmunizationsTab from "./ImmunizationsTab";
 import ProceduresTab from "./ProceduresTab";
 import "../../assets/css/PatientProfile.css";
 
-const PatientProfile = ({ patientInfo }) => {
-    // State to track the active tab
-    const [activeTab, setActiveTab] = useState("records");
-    const [isEditMode, setIsEditMode] = useState(false); // State to track edit mode
+const PatientProfile = ({ isSignedIn, contractId, wallet }) => {
+  // State to track the active tab
+  const [activeTab, setActiveTab] = useState("records");
+  const [isEditMode, setIsEditMode] = useState(false); // State to track edit mode
+  const [patientInfo, setpatientInfo] = useState({
+    full_name:"Full Name",
+    birthday:"20/02/2020",
+    blood_type:"AB+",
+    gender:"Gender",
+    records: [
+      // ... mock records data ...
+      "records 123",
+      "records1232",
+    ],
+    allergies: [
+      // ... mock allergies data ...
+      "1",
+      "2",
+      "3",
+    ],
+    immunizations: [],
+    procedures: [],
+  });
+  const [isaPatient, setisaPatient] = useState(false);
+  const localStorageData = localStorage.getItem("userinfo");
 
-    // Function to handle tab click
-    const handleTabClick = (tab) => {
-        setActiveTab(tab);
-    };
-    // Function to handle edit button click
-    const handleEditClick = () => {
-        setIsEditMode(!isEditMode);
-    };
+  //GET DETAILS OF THE PATIENT
+  const checkPatientStatus = async () => {
+    console.log("Checking the Patients status");
 
-    // Mock data for tabs
-    const mockData = {
-        records: [
-            // ... mock records data ...
-            "records 123",
-            "records1232",
-        ],
-        allergies: [
-            // ... mock allergies data ...
-            "1",
-            "2",
-            "3",
-        ],
-        immunizations: [
-        ],
-        procedures: [
-        ],
-    };
+    try {
+      // return await wallet.viewMethod({ method: 'get_patient', args: { id: wallet.accountId },contractId })
+      console.log(wallet);
+      if (localStorageData) {
+        setisaPatient(true);
+        //DEBUGING TO CHECK WHAT DATA IS STORED
+        console.log(JSON.parse(localStorageData));
+        return;
+      } else {
+        console.log("Now calling wallet");
+        // wallet.callMethod({
+        //   contractId: contractId,
+        //   method: "get_patient",
+        // })
+        // .then(async (result) => {
+        // console.log(result);
+        // localStorage.setItem("userinfo", JSON.stringify(result));
+        // });
+        wallet
+          .viewMethod({
+            contractId: contractId,
+            method: "get_patient_workaround",
+            args: {
+              account_id: await wallet.getAccountId(),
+            },
+          })
+          .then(async (result) => {
+            console.log(result);
+            setisaPatient(true);
+            setpatientInfo(result);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Function to handle tab click
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+  // Function to handle edit button click
+  const handleEditClick = () => {
+    setIsEditMode(!isEditMode);
+  };
 
-    // Use mock data if patientInfo is not provided
-    const dataToDisplay = patientInfo || mockData;
-    <h2>Patient Profile</h2>;
+  useEffect(() => {
+    if (isSignedIn && !isaPatient) checkPatientStatus();
+  }, []);
 
-    return (
+
+  return (
     <div>
-        <ul className="navigation-bar">
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-        </ul>
-        {/* Patient Information */}
-        <div className="patient-profile">
+      <ul className="navigation-bar">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+      </ul>
+      {/* Patient Information */}
+      <div className="patient-profile">
         <div className="right-section">
-            <div className="patient-info">
+          <div className="patient-info">
             {/* Display patient information */}
             {isEditMode ? (
-                <div>
+              <div>
                 <input type="text" placeholder="Name Surname" />
                 {/* I will Add more input fields for other patient info */}
-                </div>
+              </div>
             ) : (
-                <div>
-                <div>Name Surname</div>
-                <div>07/10/2000</div>
-                <div>Blood Type: A</div>
-                </div>
+              <div>
+                <div>{patientInfo.full_name}</div>
+                <div>{patientInfo.birthday}</div>
+                <div>Blood Type: {patientInfo.blood_type}</div>
+              </div>
             )}
             <button className="edit-button" onClick={handleEditClick}>
-                {isEditMode ? "Save" : "Edit"}
+              {isEditMode ? "Save" : "Edit"}
             </button>
-            </div>
+          </div>
         </div>
 
         <div className="tabs">
-            <button
+          <button
             className={
-                activeTab === "records" ? "tab-button active" : "tab-button"
+              activeTab === "records" ? "tab-button active" : "tab-button"
             }
             onClick={() => handleTabClick("records")}
-            >
+          >
             Records
-            </button>
-            <button
+          </button>
+          <button
             className={
-                activeTab === "allergies" ? "tab-button active" : "tab-button"
+              activeTab === "allergies" ? "tab-button active" : "tab-button"
             }
             onClick={() => handleTabClick("allergies")}
-            >
+          >
             Allergies
-            </button>
-            <button
+          </button>
+          <button
             className={
-                activeTab === "immunizations" ? "tab-button active" : "tab-button"
+              activeTab === "immunizations" ? "tab-button active" : "tab-button"
             }
             onClick={() => handleTabClick("immunizations")}
-            >
+          >
             Immunizations
-            </button>
-            <button
+          </button>
+          <button
             className={
-                activeTab === "procedures" ? "tab-button active" : "tab-button"
+              activeTab === "procedures" ? "tab-button active" : "tab-button"
             }
             onClick={() => handleTabClick("procedures")}
-            >
+          >
             Procedures
-            </button>
+          </button>
 
-            {/* Tab Content */}
-            {activeTab === "records" && (
-            <RecordsTab records={dataToDisplay.records} />
-            )}
-            {activeTab === "allergies" && (
-            <AllergiesTab allergies={dataToDisplay.allergies} />
-            )}
-            {activeTab === "immunizations" && (
-            <ImmunizationsTab immunizations={dataToDisplay.immunizations} />
-            )}
-            {activeTab === "procedures" && (
-            <ProceduresTab procedures={dataToDisplay.procedures} />
-            )}
+          {/* Tab Content */}
+          {activeTab === "records" && (
+            <RecordsTab records={patientInfo.records} />
+          )}
+          {activeTab === "allergies" && (
+            <AllergiesTab allergies={patientInfo.allergies} />
+          )}
+          {activeTab === "immunizations" && (
+            <ImmunizationsTab immunizations={patientInfo.immunizations} />
+          )}
+          {activeTab === "procedures" && (
+            <ProceduresTab procedures={patientInfo.procedures} />
+          )}
         </div>
-        </div>
-        <footer className="footer">
+      </div>
+      <footer className="footer">
         <p>
-            &copy; {new Date().getFullYear()} MediBridge. All rights reserved.
+          &copy; {new Date().getFullYear()} MediBridge. All rights reserved.
         </p>
-        </footer>
+      </footer>
     </div>
-    );
+  );
 };
 
 export default PatientProfile;
