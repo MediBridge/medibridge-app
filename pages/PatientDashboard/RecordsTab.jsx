4720db72c4c739c186a6e107186e90e512c2a008
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-09-05 17:24:54 -0400

    Updated the PatientRecords Tab

--- pages/PatientDashboard/RecordsTab.jsx
+++ pages/PatientDashboard/RecordsTab.jsx
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
-
-
-const RecordsTab = ({ records }) => {
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import MyContext from "../../Context/MyContext";
+import { useNavigate } from "react-router-dom";
+const RecordsTab = ({ records,isSignedIn, contractId, wallet }) => {
   const [showUploadModal, setShowUploadModal] = useState(false);
   const [condition, setCondition] = useState(""); // Add this line
   const [recordData, setRecordData] = useState(""); // Add this line
   const [date, setDate] = useState(""); // Add this line
   const [isPublic, setIsPublic] = useState("public"); // Add this line  // Mock data for records
+  const [uploadStatus, setUploadStatus] = useState(""); // STATUS OF THE FILE BEING UPLOADED TO IPFS
+  const [decryptedPDF, setDecryptedPDF] = useState(null);
+  const { documentData, updateMyData } = useContext(MyContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(documentData);
+    if(documentData)
+    navigate("/viewdata");
+  }, [documentData])
+  
   const mockRecords = [
     {
-      condition: "Fever",
-      recordData: "Some medical data about fever",
-      date: "2023-08-15",
-      isPublic: true,
+      MedicalRecord:{
+        condition: "Fever",
+        recordData: "Some medical data about fever",
+        date: "2023-08-15",
+        isPublic: true,
+      }
     },
-    {
-      condition: "Fractured Arm",
-      recordData: "Some medical data about fractured arm",
-      date: "2023-07-20",
-      isPublic: false,
-    },
+    // {
+    //   condition: "Fractured Arm",
+    //   recordData: "Some medical data about fractured arm",
+    //   date: "2023-07-20",
+    //   isPublic: false,
+    // },
     // ... add more mock records ...
   ];
 
@@ -51,14 +65,36 @@ const RecordsTab = ({ records }) => {
     setShowUploadModal(true);
     const input = document.createElement("input");
     input.type = "file";
-    input.addEventListener("change", (event) => {
+    input.addEventListener("change", async (event) => {
       const file = event.target.files[0];
       if (file) {
-        console.log("Uploaded file:", file);
+        const formData = new FormData();
+        formData.append("pdfFile", file);
+        try {
+          const response = await fetch("http://localhost:3000/fileUpload", {
+            method: "POST",
+            body: formData,
+          });
+
+          if (response.ok) {
+            const ipfsURL = await response.json();
+            setRecordData(ipfsURL);
+            console.log(recordData);
+            console.log("PDF HANDLED");
+            setUploadStatus("PDF file uploaded and processed successfully.");
+          } else {
+            setUploadStatus("Error uploading the PDF file.");
+          }
+        } catch (error) {
+          console.log(error);
+          setUploadStatus("An error occurred during the upload.");
+        }
       }
     });
     input.click();
   };
+
+  // THIS SECTION WILL UPLOAD THE DOCUMENT AND THE RELEVANT DETAILS TO THE CHAIN
   const handleUploadSubmit = () => {
     // Process the uploaded data here, e.g., send it to a server or store it locally.
     const newRecord = {
@@ -67,6 +103,27 @@ const RecordsTab = ({ records }) => {
       date,
       isPublic: isPublic === "public",
     };
+    console.log(newRecord);
+    if(recordData=="")
+    return;
+    wallet
+      .callMethod({
+        contractId: contractId,
+        method: "add_medical_record",
+        args: {
+          id: parseInt(generateUniqueId()),
+          condition:condition,
+          record_data:recordData,
+          date:date,
+          public:isPublic === "public"
+        },
+      })
+      .then(async (result) => {
+        console.log("Record has been succesfully added");
+      })
+      .catch((error) => {
+        console.log("Error while trying to upload to the chain", error);
+      });
     console.log("New Record:", newRecord);
     // Close the modal and reset the form fields
     setShowUploadModal(false);
@@ -78,78 +135,82 @@ const RecordsTab = ({ records }) => {
 
   return (
     <div>
-    <div>
-      <button className="upload-button" onClick={handleUploadClick}>
-        Upload Medical Records
-      </button>
-      {showUploadModal && (
-  <div className="upload-modal">
-    <div className="upload-form">
-      <h3>Upload Medical Records</h3>
-      <label htmlFor="condition">Condition:</label>
-      <input
-        type="text"
-        id="condition"
-        name="condition"
-        value={condition}
-        onChange={(e) => setCondition(e.target.value)}
-      />
-      <label htmlFor="recordData">Record Data:</label>
-      <textarea
+      <div>
+        <button className="upload-button" onClick={handleUploadClick}>
+          Upload Medical Records
+        </button>
+        {showUploadModal && (
+          <div className="upload-modal">
+            <div className="upload-form">
+              <h3>Upload Medical Records</h3>
+              <label htmlFor="condition">Condition:</label>
+              <input
+                type="text"
+                id="condition"
+                name="condition"
+                value={condition}
+                onChange={(e) => setCondition(e.target.value)}
+              />
+              {/* <label htmlFor="recordData">Record Data:</label> */}
+              {/* <textarea
         id="recordData"
         name="recordData"
         rows="4"
         value={recordData}
         onChange={(e) => setRecordData(e.target.value)}
-      />
-      <label htmlFor="date">Date:</label>
-      <input
-        type="date"
-        id="date"
-        name="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-      />
-      <label htmlFor="isPublic">Public/Private:</label>
-      <select
-        id="isPublic"
-        name="isPublic"
-        value={isPublic}
-        onChange={(e) => setIsPublic(e.target.value)}
-      >
-        <option value="public">Public</option>
-        <option value="private">Private</option>
-      </select>
+      /> */}
+              <label htmlFor="date">Date:</label>
+              <input
+                type="date"
+                id="date"
+                name="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+              <label htmlFor="isPublic">Public/Private:</label>
+              <select
+                id="isPublic"
+                name="isPublic"
+                value={isPublic}
+                onChange={(e) => setIsPublic(e.target.value)}
+              >
+                <option value="public">Public</option>
+                <option value="private">Private</option>
+              </select>
 
-      <button onClick={handleUploadSubmit}>Upload</button>
-    </div>
-    <div className="modal-overlay" onClick={() => setShowUploadModal(false)}></div>
-  </div>
-)}
-    </div>
-    <div className="tab-content">
-       <h3>Medical Records</h3>
-      <table className="record-table">
-        <thead>
-          <tr>
-            <th>Condition</th>
-            <th>Record Data</th>
-            <th>Date</th>
-            <th>Public/Private</th>
-          </tr>
-        </thead>
-        <tbody>
-          {recordsToDisplay.map((record, index) => (
-            <tr className="record-row" key={index}>
-              <td>{record.condition}</td>
-              <td>{record.recordData}</td>
-              <td>{record.date}</td>
-              <td>{record.isPublic ? "Public" : "Private"}</td>
+              <button onClick={handleUploadSubmit}>Upload</button>
+            </div>
+            <div
+              className="modal-overlay"
+              onClick={() => setShowUploadModal(false)}
+            ></div>
+          </div>
+        )}
+      </div>
+      <div className="tab-content">
+        <h3>Medical Records</h3>
+        <table className="record-table">
+          <thead>
+            <tr>
+              <th>Condition</th>
+              <th>Record Data</th>
+              <th>Date</th>
+              <th>Public/Private</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            { recordsToDisplay && recordsToDisplay.map((record, index) => (
+              <tr className="record-row" key={index}>
+                <td>{ record.MedicalRecord.condition}</td>
+                <td><div onClick={e=> getDocument(record.MedicalRecord.record_data) } >Document</div> </td>
+                {/* <td>{decryptedPDF && <iframe title="Decrypted PDF" src={decryptedPDF} width="100%" height="600px" />}</td> */}
+                <td>{record.MedicalRecord.date}</td>
+                <td>{record.MedicalRecord.isPublic ? "Public" : "Private"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
