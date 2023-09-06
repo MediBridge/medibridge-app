2039653dce4398005f3913afc66533724293182e
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-09-06 09:16:28 -0400

    Added Immunizations Tab and Filled with Mock data

--- pages/PatientDashboard/ImmunizationsTab.jsx
+++ pages/PatientDashboard/ImmunizationsTab.jsx
@@ -1,15 +1,119 @@
-// ImmunizationsTab.js
-import React from "react";
+import React, { useState } from "react";
 
-const ImmunizationsTab = ({ immunizations }) => {
+const ImmunizationsTab = () => {
+  const [showUploadModal, setShowUploadModal] = useState(false);
+  const [vaccine, setVaccine] = useState("");
+  const [date, setDate] = useState("");
+  const [provider, setProvider] = useState("");
+  const [uploadStatus, setUploadStatus] = useState("");
+  const [immunizations, setImmunizations] = useState([
+    // Mock immunization data
+    {
+      vaccine: "COVID-19 Vaccine",
+      date: "2023-01-15",
+      provider: "Health Clinic A",
+    },
+    {
+      vaccine: "Flu Shot",
+      date: "2022-10-05",
+      provider: "Health Clinic B",
+    },
+    // Add more mock data as needed
+  ]);
+
+  // Function to handle the click event for uploading immunization records
+  const handleUploadClick = () => {
+    setShowUploadModal(true);
+  };
+
+  // Function to handle the submission of the immunization record form
+  const handleUploadSubmit = () => {
+    // Process the uploaded data here, e.g., send it to a server or store it locally.
+    const newImmunization = {
+      vaccine,
+      date,
+      provider,
+    };
+    console.log(newImmunization);
+    // @prakhar728 Add code to handle the upload to the chain or any other processing
+
+    // Close the modal and reset the form fields
+    setShowUploadModal(false);
+    setVaccine("");
+    setDate("");
+    setProvider("");
+    setUploadStatus("Immunization record uploaded successfully.");
+
+    // Add the new immunization data to the table
+    setImmunizations([...immunizations, newImmunization]);
+  };
+
   return (
-    <div className="tab-content">
-      <h3>Immunizations</h3>
-      <ul>
-        {immunizations.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+    <div>
+      <div>
+        <button className="upload-button" onClick={handleUploadClick}>
+          Upload Immunization Record
+        </button>
+        {showUploadModal && (
+          <div className="upload-modal">
+            <div className="upload-form">
+              <h3>Upload Immunization Record</h3>
+              <label htmlFor="vaccine">Vaccine:</label>
+              <input
+                type="text"
+                id="vaccine"
+                name="vaccine"
+                value={vaccine}
+                onChange={(e) => setVaccine(e.target.value)}
+              />
+              <label htmlFor="date">Date:</label>
+              <input
+                type="date"
+                id="date"
+                name="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+              <label htmlFor="provider">Provider:</label>
+              <input
+                type="text"
+                id="provider"
+                name="provider"
+                value={provider}
+                onChange={(e) => setProvider(e.target.value)}
+              />
+
+              <button onClick={handleUploadSubmit}>Upload</button>
+            </div>
+            <div
+              className="modal-overlay"
+              onClick={() => setShowUploadModal(false)}
+            ></div>
+          </div>
+        )}
+      </div>
+
+      <div className="tab-content">
+        <h3>Immunization Records</h3>
+        <table className="immunizations-table">
+          <thead>
+            <tr>
+              <th>Vaccine</th>
+              <th>Date</th>
+              <th>Provider</th>
+            </tr>
+          </thead>
+          <tbody>
+            {immunizations.map((immunization, index) => (
+              <tr className="immunization-row" key={index}>
+                <td>{immunization.vaccine}</td>
+                <td>{immunization.date}</td>
+                <td>{immunization.provider}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
